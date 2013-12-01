18415abbc5499e4419817b10252a8ff7953d9cb5
Antoine Jackson <a.j.william26@gmail.com>
2013-12-01 21:55:25 +0100

    + Added methods to get durations

--- Public/Models/Event.js
+++ Public/Models/Event.js
@@ -22,6 +22,50 @@ define([
         setEnd: function (date) {
             var base = this;
             base.set("end", date.toISOString());
+        },
+        getDuration: function (start, end) {
+            var base = this;
+            var calc_start = start > base.getStart() ? start : base.getStart();
+            var calc_end = end < base.getEnd() ? end : base.getEnd();
+            var duration = calc_end.getTime() - calc_start.getTime();
+            if (duration > 0) {
+                return duration;
+            } else {
+                return 0;
+            }
+        },
+        getDoneDuration: function (start, end) {
+            var base = this;
+            var now = new Date();
+            if (now < end) {
+                end = now;
+            }
+            var base = this;
+            var calc_start = start > base.getStart() ? start : base.getStart();
+            var calc_end = end < base.getEnd() ? end : base.getEnd();
+            var duration = calc_end.getTime() - calc_start.getTime();
+            if (duration > 0) {
+                return duration;
+            } else {
+                return 0;
+            }
+
+        },
+        getLeftDuration: function (start, end) {
+            var base = this;
+            var now = new Date();
+            if (now > start) {
+                start = now;
+            }
+            var base = this;
+            var calc_start = start > base.getStart() ? start : base.getStart();
+            var calc_end = end < base.getEnd() ? end : base.getEnd();
+            var duration = calc_end.getTime() - calc_start.getTime();
+            if (duration > 0) {
+                return duration;
+            } else {
+                return 0;
+            }
         }
     });
     return Model;
